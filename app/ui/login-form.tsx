81dce12377c10dba15fb55e2af6d8f09538af270
bach9w <78446671+bach9w@users.noreply.github.com>
2023-11-07 18:55:33 +0200

    aa

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { authenticate } from '@/app/lib/actions';
 import { lusitana } from '@/app/ui/fonts';
 import {
 	AtSymbolIcon,
@@ -9,10 +10,11 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '../lib/actions';
 
 export default function LoginForm() {
 	const [code, action] = useFormState(authenticate, undefined);
+	const { pending } = useFormStatus();
+
 	return (
 		<form action={action} className="space-y-3">
 			<div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -62,11 +64,11 @@ export default function LoginForm() {
 				</div>
 				<LoginButton />
 				<div className="flex h-8 items-end space-x-1">
-					{code === 'CredentialSignin' && (
+					{code === 'CredentialsSignin' && (
 						<>
 							<ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-							<p aria-live="polite" className="text-red-500 text-sm">
-								Invalid email or password.
+							<p aria-live="polite" className="text-sm text-red-500">
+								Invalid credentials
 							</p>
 						</>
 					)}
@@ -78,6 +80,7 @@ export default function LoginForm() {
 
 function LoginButton() {
 	const { pending } = useFormStatus();
+
 	return (
 		<Button className="mt-4 w-full" aria-disabled={pending}>
 			Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
